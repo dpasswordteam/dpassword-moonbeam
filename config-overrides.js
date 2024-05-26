2dff04a8ab3893abf997c7012fa4a410371099ae
Matias Péres <matiasperes23@gmail.com>
2024-05-26 03:33:29 +0200

    CORS

--- config-overrides.js
+++ config-overrides.js
@@ -1,18 +1,37 @@
 const webpack = require('webpack');
 const { override, addWebpackAlias, addWebpackPlugin } = require('customize-cra');
-const path = require('path');
 
-module.exports = override(
-  addWebpackAlias({
-    http: require.resolve('stream-http'),
-    https: require.resolve('https-browserify'),
-    crypto: require.resolve('crypto-browserify'),
-    buffer: require.resolve('buffer')
-  }),
-  addWebpackPlugin(
-    new webpack.ProvidePlugin({
-      Buffer: ['buffer', 'Buffer'],
-      process: 'process/browser'
-    })
-  )
-);
+module.exports = {
+    webpack: override(
+        addWebpackAlias({
+          http: require.resolve('stream-http'),
+          https: require.resolve('https-browserify'),
+          crypto: require.resolve('crypto-browserify'),
+          buffer: require.resolve('buffer')
+        }),
+        addWebpackPlugin(
+          new webpack.ProvidePlugin({
+            Buffer: ['buffer', 'Buffer'],
+            process: 'process/browser'
+          })
+        )
+    ),
+    // Extend/override the dev server configuration used by CRA
+    // See: https://github.com/timarney/react-app-rewired#extended-configuration-options
+    devServer: function(configFunction) {
+        return function(proxy, allowedHost) {
+        // Create the default config by calling configFunction with the proxy/allowedHost parameters
+        // Default config: https://github.com/facebook/create-react-app/blob/master/packages/react-scripts/config/webpackDevServer.config.js
+        const config = configFunction(proxy, allowedHost);
+
+        // Set loose allow origin header to prevent CORS issues
+        config.headers = {'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers': 'X-Requested-With, Content-Type, Authorization',
+        }
+
+        return config;
+        };
+    },
+};
+
